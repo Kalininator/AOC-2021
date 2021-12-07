9cf4785decf11dc31381a8e7251891064f01f1f2
Alexander Kalinin <alex@kalinin.uk>
2021-12-07 19:05:07 +0000

    7

---
+++ day7/src/main.rs
@@ -0,0 +1,58 @@
+fn part_one(crabs: &Vec<i32>) -> i32 {
+    let mut sorted_crabs = crabs.clone();
+    sorted_crabs.sort();
+    let average_position = sorted_crabs[crabs.len() / 2];
+    sorted_crabs
+        .iter()
+        .map(|c| (c - average_position).abs())
+        .sum()
+}
+
+fn part_two(crabs: &Vec<i32>) -> i32 {
+    let mut sorted_crabs = crabs.clone();
+    sorted_crabs.sort();
+    let mut costs: Vec<i32> = vec![];
+    let min = sorted_crabs.iter().copied().min().unwrap();
+    let max = sorted_crabs.iter().copied().max().unwrap();
+    for position in min..=max {
+        costs.push(
+            crabs
+                .iter()
+                .map(|crab| calculate_cost((position - crab).abs()))
+                .sum(),
+        );
+    }
+    *costs.iter().min().unwrap()
+}
+
+fn calculate_cost(distance: i32) -> i32 {
+    let mut acc: i32 = 0;
+    for i in 0..=distance {
+        acc += i;
+    }
+    acc
+}
+
+fn parse_crabs(line: &str) -> Vec<i32> {
+    line.split(',').map(|c| c.parse().unwrap()).collect()
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let file_lines = utils::read_file(&args[1]);
+    let crabs = parse_crabs(file_lines.first().unwrap());
+    println!("Part one result: {}", part_one(&crabs));
+    println!("Part two result: {}", part_two(&crabs));
+}
+
+#[test]
+fn part_one_test() {
+    let crabs = parse_crabs(&String::from("16,1,2,0,4,2,7,1,2,14"));
+    assert_eq!(part_one(&crabs), 37);
+}
+
+#[test]
+fn part_two_test() {
+    let crabs = parse_crabs(&String::from("16,1,2,0,4,2,7,1,2,14"));
+    assert_eq!(part_two(&crabs), 168);
+}
