1768c25e42698bf1fa91cb05969832ad6f9e7056
Alexander Kalinin <alex@kalinin.uk>
2021-12-09 21:59:46 +0000

    p2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 struct Heightmap {
     map: Vec<Vec<u32>>,
     width: i32,
@@ -39,6 +41,49 @@ impl Heightmap {
             && self.get_point(x, y - 1).unwrap_or(10) > val
             && self.get_point(x, y + 1).unwrap_or(10) > val
     }
+
+    fn get_all_points(&self) -> Vec<(i32, i32)> {
+        let mut points: Vec<(i32, i32)> = vec![];
+        for x in 0..self.width {
+            for y in 0..self.height {
+                points.push((x, y));
+            }
+        }
+        points
+    }
+
+    fn get_adjacent_points(&self, x: i32, y: i32) -> Vec<(i32, i32)> {
+        let mut points: Vec<(i32, i32)> = vec![];
+        if x > 0 {
+            points.push((x - 1, y));
+        }
+        if x < self.width - 1 {
+            points.push((x + 1, y));
+        }
+        if y > 0 {
+            points.push((x, y - 1));
+        }
+        if y < self.height - 1 {
+            points.push((x, y + 1));
+        }
+        points
+    }
+
+    fn get_basin_from_floor(&self, x: i32, y: i32) -> Vec<(i32, i32)> {
+        let mut basin: Vec<(i32, i32)> = vec![];
+        let val = self.get_point(x, y).unwrap();
+        // add start point
+        basin.push((x, y));
+        let mut adjacents = self.get_adjacent_points(x, y);
+        adjacents.retain(|p| {
+            let v = self.get_point(p.0, p.1).unwrap();
+            v > val && v < 9
+        });
+        for p in adjacents {
+            basin.append(&mut self.get_basin_from_floor(p.0, p.1));
+        }
+        dedupe_points(basin)
+    }
 }
 
 fn find_low_points(map: &Heightmap) -> Vec<u32> {
@@ -50,18 +95,40 @@ fn find_low_points(map: &Heightmap) -> Vec<u32> {
             }
         }
     }
-    // for row in map.map {
-    //     for column in row {
-    //         if map.is_low_point(column, row) {
-    //             low_points.push(map.get_point(column, row).unwrap());
-    //         }
-    //     }
-    // }
     low_points
 }
 
 fn part_one(map: &Heightmap) -> u32 {
     find_low_points(map).iter().map(|i| i + 1).sum()
+}
+
+fn part_two(map: &Heightmap) -> usize {
+    let mut points = map.get_all_points();
+    // All points 9 or higher are not part of a basin
+    points.retain(|p| map.get_point(p.0, p.1).unwrap() < 9);
+
+    let mut basins: Vec<usize> = vec![];
+
+    loop {
+        let basin = map.get_basin_from_floor(points[0].0, points[0].1);
+        points.retain(|p| !basin.contains(p));
+        basins.push(basin.len());
+        if points.is_empty() {
+            break;
+        }
+    }
+
+    basins.sort_unstable();
+    basins.reverse();
+    basins[0] * basins[1] * basins[2]
+}
+
+fn dedupe_points(points: Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+    let mut set: HashSet<(i32, i32)> = HashSet::new();
+    for p in points {
+        set.insert(p);
+    }
+    set.into_iter().collect()
 }
 
 fn main() {
@@ -72,6 +139,8 @@ fn main() {
 
     let p1 = part_one(&heightmap);
     println!("Part one result: {}", p1);
+    let p2 = part_two(&heightmap);
+    println!("Part two result: {}", p2);
 }
 
 #[test]
@@ -85,4 +154,17 @@ fn part_one_test() {
     ];
     let map = Heightmap::new(&input);
     assert_eq!(part_one(&map), 15);
+}
+
+#[test]
+fn part_two_test() {
+    let input: Vec<String> = vec![
+        String::from("2199943210"),
+        String::from("3987894921"),
+        String::from("9856789892"),
+        String::from("8767896789"),
+        String::from("9899965678"),
+    ];
+    let map = Heightmap::new(&input);
+    assert_eq!(part_two(&map), 1134);
 }
