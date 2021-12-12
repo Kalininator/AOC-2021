7d1cb34de6945057a79e6dc621020d20f19d6e83
Alexander Kalinin <alex@kalinin.uk>
2021-12-12 16:07:41 +0000

    Day10 (#11)
    * day 10 part 1
    
    * part 2
    
    * add test and rename func

---
+++ day10/src/main.rs
@@ -0,0 +1,99 @@
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let lines = utils::read_file(&args[1]);
+    println!("Part one: {}", part_one(&lines));
+    println!("Part two: {}", part_two(&lines));
+}
+
+#[derive(Debug, PartialEq)]
+enum Score {
+    Illegal(u128),
+    Incomplete(u128),
+}
+
+fn get_line_score(line: &str) -> Score {
+    let mut chars: Vec<char> = vec![];
+
+    for char in line.chars() {
+        match char {
+            '(' => chars.push('('),
+            '[' => chars.push('['),
+            '{' => chars.push('{'),
+            '<' => chars.push('<'),
+            ')' => {
+                let last = chars.pop().unwrap();
+                if last != '(' {
+                    return Score::Illegal(3);
+                }
+            }
+            ']' => {
+                let last = chars.pop().unwrap();
+                if last != '[' {
+                    return Score::Illegal(57);
+                }
+            }
+            '}' => {
+                let last = chars.pop().unwrap();
+                if last != '{' {
+                    return Score::Illegal(1197);
+                }
+            }
+            '>' => {
+                let last = chars.pop().unwrap();
+                if last != '<' {
+                    return Score::Illegal(25137);
+                }
+            }
+            _ => panic!("fo"),
+        }
+    }
+    let mut acc: u128 = 0;
+    for c in chars.iter().rev() {
+        acc *= 5;
+        acc += match c {
+            '(' => 1,
+            '[' => 2,
+            '{' => 3,
+            '<' => 4,
+            _ => panic!("foo"),
+        }
+    }
+    Score::Incomplete(acc)
+}
+
+fn part_one(lines: &[String]) -> u128 {
+    let mut acc: u128 = 0;
+    for score in lines.iter().map(|l| get_line_score(l)) {
+        if let Score::Illegal(val) = score {
+            acc += val
+        }
+    }
+    acc
+}
+
+fn part_two(lines: &[String]) -> u128 {
+    let mut scores: Vec<u128> = lines
+        .iter()
+        .map(|l| get_line_score(l))
+        .filter_map(|score| {
+            if let Score::Incomplete(val) = score {
+                return Some(val);
+            }
+            None
+        })
+        .collect();
+    scores.sort_unstable();
+    scores[scores.len() / 2]
+}
+
+#[test]
+fn first_illegal_square() {
+    let input = "[[<[([]))<([[{}[[()]]]";
+    assert_eq!(get_line_score(input), Score::Illegal(3));
+}
+
+#[test]
+fn incomplete() {
+    let input = "<{([{{}}[<[[[<>{}]]]>[]]";
+    assert_eq!(get_line_score(input), Score::Incomplete(294));
+}
