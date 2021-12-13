0972907fe026f42305e624e6e004e74eda840b78
Alexander Kalinin <alex@kalinin.uk>
2021-12-13 18:14:52 +0000

    refactor for both parts to work together

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -14,32 +14,68 @@ fn main() {
         let right_entry = map.entry(right.clone()).or_insert_with(Vec::new);
         right_entry.push(left.clone());
     }
+    part_1(&map);
+    part_2(&map);
+}
 
-    println!("{:?}", map);
-
-    let mut paths = get_all_paths(&map);
+fn part_1(map: &HashMap<Cave, Vec<Cave>>) {
+    let mut paths = get_all_paths(map, SmallCaveBehaviour::OnlyOnce);
     paths.retain(|p| p.last() == Some(&Cave::End));
-
-    println!("Paths to end: {}", paths.len());
-    //
-    //     for path in paths {
-    //         println!("Path: {:?}", path);
-    //     }
+    println!("Part 1: {}", paths.len());
 }
 
-fn get_all_paths(map: &HashMap<Cave, Vec<Cave>>) -> Vec<Vec<Cave>> {
-    get_paths_to_end(map, Cave::Start, vec![])
+fn part_2(map: &HashMap<Cave, Vec<Cave>>) {
+    let mut paths = get_all_paths(map, SmallCaveBehaviour::OneCaveTwice);
+    paths.retain(|p| p.last() == Some(&Cave::End));
+    println!("Part 2: {}", paths.len());
 }
 
-fn can_visit_cave(path: &[Cave], cave: Cave) -> bool {
+#[derive(Clone, Copy)]
+enum SmallCaveBehaviour {
+    OnlyOnce,
+    OneCaveTwice,
+}
+
+fn get_all_paths(
+    map: &HashMap<Cave, Vec<Cave>>,
+    small_cave_behaviour: SmallCaveBehaviour,
+) -> Vec<Vec<Cave>> {
+    get_paths_to_end(map, Cave::Start, vec![], small_cave_behaviour)
+}
+
+fn has_multiple_same_small_cave(path: &[Cave]) -> bool {
+    let mut caves_hash = HashMap::new();
+    for cave in path {
+        if let Cave::Small(val) = cave {
+            let entry = caves_hash.entry(val).or_insert(0);
+            *entry += 1;
+        }
+    }
+    caves_hash.values().any(|&x| x > 1)
+}
+
+fn can_visit_cave(path: &[Cave], cave: Cave, small_cave_behaviour: SmallCaveBehaviour) -> bool {
     match cave {
-        Cave::Small(_) => !path.contains(&cave),
+        // // Part 1
+        // Cave::Small(_) => !path.contains(&cave),
+        // Part 2
+        Cave::Small(_) => {
+            if let SmallCaveBehaviour::OneCaveTwice = small_cave_behaviour {
+                return !path.contains(&cave) || !has_multiple_same_small_cave(path);
+            }
+            !path.contains(&cave)
+        }
         Cave::Start => false,
         _ => true,
     }
 }
 
-fn get_paths_to_end(map: &HashMap<Cave, Vec<Cave>>, from: Cave, path: Vec<Cave>) -> Vec<Vec<Cave>> {
+fn get_paths_to_end(
+    map: &HashMap<Cave, Vec<Cave>>,
+    from: Cave,
+    path: Vec<Cave>,
+    small_cave_behaviour: SmallCaveBehaviour,
+) -> Vec<Vec<Cave>> {
     // println!("Path {:?} moving to {:?}", path, from);
     // If reached end then we good
     if let Cave::End = from {
@@ -47,13 +83,20 @@ fn get_paths_to_end(map: &HashMap<Cave, Vec<Cave>>, from: Cave, path: Vec<Cave>)
     }
     let mut links = map.get(&from).unwrap_or(&vec![]).clone();
     // remove small caves that have been visited
-    links.retain(|link| can_visit_cave(&path, link.clone()));
+    links.retain(|link| can_visit_cave(&path, link.clone(), small_cave_behaviour));
     if links.is_empty() {
         return vec![path];
     }
     links
         .iter()
-        .map(|l| get_paths_to_end(map, l.clone(), [path.clone(), vec![l.clone()]].concat()))
+        .map(|l| {
+            get_paths_to_end(
+                map,
+                l.clone(),
+                [path.clone(), vec![l.clone()]].concat(),
+                small_cave_behaviour,
+            )
+        })
         .collect::<Vec<Vec<Vec<Cave>>>>()
         .concat()
 }
@@ -94,4 +137,16 @@ impl FromStr for Cave {
             }
         }
     }
+}
+
+#[test]
+fn has_multiple_same_small_cave_test() {
+    assert_eq!(
+        has_multiple_same_small_cave(&[
+            Cave::Small("abc".to_string()),
+            Cave::Small("abc".to_string()),
+            Cave::Small("foo".to_string())
+        ]),
+        true
+    );
 }
