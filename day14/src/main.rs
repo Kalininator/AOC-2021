440c58c6f37194757c651463f566b5ec92ee0267
Alexander Kalinin <alex@kalinin.uk>
2021-12-14 16:48:40 +0000

    optimise and run part 2

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let mut lines = utils::read_file(&args[1]);
 
-    let mut chars: Vec<char> = lines
+    let chars: Vec<char> = lines
         .first()
         .expect("First line not found")
         .chars()
@@ -23,42 +23,68 @@ fn main() {
         );
     }
 
-    print_chars(&chars);
-    for i in 0..40 {
-        println!("Running step: {}", i + 1);
-        step(&mut chars, &insertion_rules);
-        if i == 9 {
-            println!("Part 1 result: {}", count_score(&chars));
-        }
+    let mut pair_map: HashMap<String, u128> = HashMap::new();
+
+    for pair_string in chars.windows(2) {
+        let entry = pair_map.entry(pair_string.iter().collect()).or_insert(0);
+        *entry += 1;
     }
-    println!("Part 2 result: {}", count_score(&chars));
+
+    print_chars(&chars);
+    println!("{:?}", pair_map);
+    for _i in 0..10 {
+        step_pairs(&mut pair_map, &insertion_rules);
+    }
+
+    println!("Part 1 result: {}", count_from_map(&chars, &pair_map));
+    for _i in 0..30 {
+        step_pairs(&mut pair_map, &insertion_rules);
+    }
+    println!("Part 2 result: {}", count_from_map(&chars, &pair_map));
 }
 
 fn print_chars(chars: &[char]) {
     println!("{}", chars.iter().collect::<String>());
 }
 
-fn count_score(chars: &[char]) -> u128 {
-    let mut map: HashMap<char, u128> = HashMap::new();
-    for c in chars {
-        let entry = map.entry(*c).or_insert(0);
-        *entry += 1;
-    }
-    map.values().max().unwrap() - map.values().min().unwrap()
-}
-
-fn step(chars: &mut Vec<char>, insertion_rules: &HashMap<String, char>) {
-    let pairs: Vec<String> = chars.windows(2).map(|w| w.iter().collect()).collect();
-    let vals_to_insert: Vec<Option<&char>> = pairs
-        .iter()
-        .map(|pair| insertion_rules.get(pair))
-        .collect::<Vec<Option<&char>>>();
-    let existing_values: Vec<char> = chars.drain(..).collect();
-    for i in 0..vals_to_insert.len() {
-        chars.push(existing_values[i]);
-        if let Some(val) = vals_to_insert[i] {
-            chars.push(*val);
+fn count_from_map(original_formula: &[char], map: &HashMap<String, u128>) -> u128 {
+    let mut count: HashMap<char, u128> = HashMap::new();
+    for pair in map {
+        if *pair.1 == 0 {
+            continue;
+        }
+        for c in pair.0.chars() {
+            let entry = count.entry(c).or_insert(0);
+            *entry += pair.1;
         }
     }
-    chars.push(*existing_values.last().unwrap());
+    *(count.entry(*original_formula.last().unwrap()).or_insert(0)) += 1;
+    (count.values().max().unwrap() - count.values().min().unwrap()) / 2
+}
+
+fn step_pairs(pairs: &mut HashMap<String, u128>, insertion_rules: &HashMap<String, char>) {
+    let mut new_pairs: HashMap<String, u128> = HashMap::new();
+    for pair in pairs.iter_mut() {
+        let new_char = insertion_rules.get(pair.0);
+        if let Some(c) = new_char {
+            let count = *pair.1;
+            *pair.1 = 0;
+            let left_pair: String =
+                vec![*pair.0.chars().collect::<Vec<char>>().get(0).unwrap(), *c]
+                    .iter()
+                    .collect();
+            let right_pair: String =
+                vec![*c, *pair.0.chars().collect::<Vec<char>>().get(1).unwrap()]
+                    .iter()
+                    .collect();
+            let left_entry = new_pairs.entry(left_pair).or_insert(0);
+            *left_entry += count;
+            let right_entry = new_pairs.entry(right_pair).or_insert(0);
+            *right_entry += count;
+        }
+    }
+    for new_pair in new_pairs {
+        let entry = pairs.entry(new_pair.0).or_insert(0);
+        *entry += new_pair.1;
+    }
 }
