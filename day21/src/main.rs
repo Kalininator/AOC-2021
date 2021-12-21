7d0cb1870494c58e5c0d8d870bb4e2d5a9f963f7
Alexander Kalinin <alex@kalinin.uk>
2021-12-21 21:55:01 +0000

    p2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
+
 struct Dice {
-    last_roll: u32,
-    rolls: u32,
+    last_roll: u128,
+    rolls: u128,
 }
 
 impl Dice {
@@ -11,7 +13,7 @@ impl Dice {
         }
     }
 
-    fn next(&mut self) -> u32 {
+    fn next(&mut self) -> u128 {
         let next = if self.last_roll == 100 {
             1
         } else {
@@ -23,22 +25,19 @@ impl Dice {
     }
 }
 
-fn move_places(place: &mut u32, places: u32) {
+fn move_places(place: &mut u128, places: u128) {
     *place += places;
     while *place > 10 {
         *place -= 10;
     }
 }
-
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let lines = utils::read_file(&args[1]);
-    let mut p1_place = sscanf::scanf!(lines[0], "Player 1 starting position: {}", u32).unwrap();
-    let mut p2_place = sscanf::scanf!(lines[1], "Player 2 starting position: {}", u32).unwrap();
+fn part_one(lines: &[String]) -> u128 {
+    let mut p1_place = sscanf::scanf!(lines[0], "Player 1 starting position: {}", u128).unwrap();
+    let mut p2_place = sscanf::scanf!(lines[1], "Player 2 starting position: {}", u128).unwrap();
 
     let mut dice = Dice::new();
-    let mut p1_score = 0u32;
-    let mut p2_score = 0u32;
+    let mut p1_score = 0u128;
+    let mut p2_score = 0u128;
 
     loop {
         // p1 go
@@ -46,8 +45,7 @@ fn main() {
         p1_score += p1_place;
         if p1_score >= 1000 {
             println!("P1 Won");
-            println!("Part One: {}", p2_score * dice.rolls);
-            break;
+            return p2_score * dice.rolls;
         }
 
         // p2 go
@@ -55,8 +53,91 @@ fn main() {
         p2_score += p2_place;
         if p2_score >= 1000 {
             println!("P2 Won");
-            println!("Part One: {}", p1_score * dice.rolls);
-            break;
+            return p1_score * dice.rolls;
         }
     }
+}
+
+#[derive(Clone, Hash, PartialEq, Eq)]
+struct GameState {
+    p1_score: u128,
+    p2_score: u128,
+    p1_place: u128,
+    p2_place: u128,
+}
+
+static TRIPLE_ROLL_RESULTS: &[(u128, u128)] =
+    &[(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)];
+fn modulo(mut value: u128, min: u128, max: u128) -> u128 {
+    if value >= max {
+        let range = max - min;
+        let q = 1 + (value - max) / range;
+        value -= q * range;
+    }
+
+    value
+}
+
+fn solve_recursive(state: GameState, cache: &mut HashMap<GameState, (u128, u128)>) -> (u128, u128) {
+    if cache.contains_key(&state) {
+        return *cache.get(&state).unwrap();
+    }
+
+    if state.p1_score >= 21 {
+        return (1, 0);
+    }
+    if state.p2_score >= 21 {
+        return (0, 1);
+    }
+
+    let mut total_p1_wins = 0;
+    let mut total_p2_wins = 0;
+
+    for (roll, freq) in TRIPLE_ROLL_RESULTS {
+        let new_position = modulo(state.p1_place + roll, 1, 11);
+        let new_score = state.p1_score + new_position;
+
+        let (p2_wins, p1_wins) = solve_recursive(
+            GameState {
+                p1_score: state.p2_score,
+                p2_score: new_score,
+                p1_place: state.p2_place,
+                p2_place: new_position,
+            },
+            cache,
+        );
+        total_p1_wins += freq * p1_wins;
+        total_p2_wins += freq * p2_wins;
+    }
+
+    cache.insert(state, (total_p1_wins, total_p2_wins));
+
+    (total_p1_wins, total_p2_wins)
+}
+
+fn part_two(lines: &[String]) -> u128 {
+    let p1_place = sscanf::scanf!(lines[0], "Player 1 starting position: {}", u128).unwrap();
+    let p2_place = sscanf::scanf!(lines[1], "Player 2 starting position: {}", u128).unwrap();
+    let (p1_wins, p2_wins) = solve_recursive(
+        GameState {
+            p1_score: 0,
+            p2_score: 0,
+            p1_place,
+            p2_place,
+        },
+        &mut HashMap::new(),
+    );
+    if p1_wins > p2_wins {
+        p1_wins
+    } else {
+        p2_wins
+    }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let lines = utils::read_file(&args[1]);
+
+    println!("Part one: {}", part_one(&lines));
+    println!("Part two: {}", part_two(&lines));
 }
